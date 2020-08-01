b72a94556c4d7ef98c3cbe7d6ed9cdf4f77e9036
Huamin Ni <imsophieni@gmail.com>
2020-07-31 17:32:32 -0700

    adjusted to mvc pattern

---
+++ Answer-Questions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Answer-Questions
+//
+//  Created by huaminni on 7/31/20.
+//  Copyright © 2020 Huamin Ni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
